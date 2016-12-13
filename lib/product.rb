dc4b8fb1c08869261a8affd737d6088d46f1571d
Jonathan Thom and Tyler Ferrin <jonathan@>
2016-12-13 09:00:04 -0800

    Add not purchased method to product

--- lib/product.rb
+++ lib/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
   belongs_to(:purchase)
   validates(:name, {:presence => true, :length => {:maximum => 20}})
+
+  scope(:not_purchased, -> do
+    where({:purchased => false})
+  end)
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -7,4 +7,28 @@ describe(Product) do
     product = Product.create(:name => "")
     expect(product.save).to(eq(false))
   end
+
+  it('tests the max length of the name') do
+    product = Product.create(:name => 'a'.*(21))
+    expect(product.save).to(eq(false))
+  end
+
+  it('tests the presence of the description') do
+    product = Product.create(:description => '')
+    expect(product.save).to(eq(false))
+  end
+
+  it('tests the presence of the price') do
+    product = Product.create(:price => '')
+    expect(product.save).to(eq(false))
+  end
+
+  describe('.not_purchased') do
+    it('returns a list of products not purchased yet') do
+      product = Product.create(:name => 'name', :description => 'description', :purchased => false)
+      product2 = Product.create(:name => 'name', :description => 'description', :purchased => false)
+      product3 = Product.create(:name => 'name', :description => 'description', :purchased => true)
+      expect(Product.not_purchased()).to(eq([product, product2]))
+    end
+  end
 end
