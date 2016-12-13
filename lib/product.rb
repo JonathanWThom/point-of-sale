725bbe57761830828f8c1852585f74274046a873
Jonathan Thom and Tyler Ferrin <jonathan+tyler@>
2016-12-13 08:49:01 -0800

    add validator to product class

--- lib/product.rb
+++ lib/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
   belongs_to(:purchase)
+  validates(:name, {:presence => true, :length => {:maximum => 20}})
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -2,4 +2,9 @@ require('spec_helper')
 
 describe(Product) do
   it {should belong_to(:purchase)}
+
+  it("tests presence of name attribute") do
+    product = Product.create(:name => "")
+    expect(product.save).to(eq(false))
+  end
 end
