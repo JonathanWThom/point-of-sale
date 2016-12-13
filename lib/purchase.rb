fee79f2296f94b0c6cd152759e93d9d9041da0fc
Jonathan Thom and Tyler Ferrin <tyler@>
2016-12-13 10:19:18 -0800

    Will total purchases for a date range

--- lib/purchase.rb
+++ lib/purchase.rb
@@ -1,6 +1,21 @@
 class Purchase < ActiveRecord::Base
   has_many(:products)
 
+  # scope(:created_between -> do |start_date, end_date|
+  #  where({:date_of_purchase => start_date..end_date}
+  # end)
+
+  # scope(:created_at -> do
+  #   where(:date_of_purchase >=)
+  # end)
+  #
+  # Comment.where(:created_at => @selected_date.beginning_of_day..@selected_date.end_of_day)
+
+  scope(:not_purchased, -> do
+    where({:purchased => false})
+  end)
+
+
   def total
     total = 0
     self.products.each() do |product|
@@ -8,4 +23,14 @@ class Purchase < ActiveRecord::Base
     end
     total
   end
+
+  def self.purchases_total(day1, day2)
+    purchases = Purchase.where(date_of_purchase: day1..day2)
+    total = 0
+    purchases.each() do |purchase|
+      total += purchase.total
+    end
+    total
+  end
+
 end
--- spec/purchase_spec.rb
+++ spec/purchase_spec.rb
@@ -11,4 +11,18 @@ describe(Purchase) do
       expect(purchase.total).to(eq(8.0))
     end
   end
+
+  describe(".purchases_total") do
+    it("adds up purchases between a given date range") do
+      purchase = Purchase.create(:date_of_purchase => Date.today)
+      purchase1date = purchase.date_of_purchase
+      product = purchase.products.create(:name => 'costco_dog', :description => 'description', :price => 3.50, :purchased => true)
+      product2 = purchase.products.create(:name => 'seattle_dog', :description => 'description', :price => 4.50, :purchased => true)
+      purchase2 = Purchase.create(:date_of_purchase => Date.new(2016, 12, 25))
+      purchase2date = purchase2.date_of_purchase
+      product3 = purchase2.products.create(:name => 'weener_dog', :description => 'description', :price => 5.5, :purchased => true)
+      product4 = purchase2.products.create(:name => 'up_dog', :description => 'description', :price => 4.50, :purchased => true)
+      expect(Purchase.purchases_total(purchase1date, purchase2date)).to(eq(18))
+    end
+  end
 end
